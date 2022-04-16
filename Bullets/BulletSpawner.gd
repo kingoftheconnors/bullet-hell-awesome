78849a35212d38eedbf0537ac3d2bf46dc035a9a
kingoftheconnors <Connormcp777@gmail.com>
2022-04-15 21:04:21 -0400

    Add V-bullet cones

--- Bullets/BulletSpawner.gd
+++ Bullets/BulletSpawner.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 export(Vector2) var default_direction = Vector2.DOWN
 export(float) var bulletsPerSecond = 1
+export(float) var v_angle_step = 10
 var timeSinceLastBullet : float = 0
 
 func _process(delta):
@@ -13,7 +14,7 @@ func _process(delta):
 
 # direction is a paramater that can be passed in, but if it isn't, it'll
 # use the default_direction variable
-func shoot(direction = default_direction):
+func shoot(direction : Vector2 = default_direction):
 	var bullet = preload("res://Bullets/Bullet.tscn").instance()
 	bullet.direction = direction
 	get_tree().root.get_node("Level").add_child(bullet)
@@ -25,3 +26,17 @@ func shoot_at_player():
 	bullet.direction = (player.global_position - global_position).normalized()
 	get_tree().root.get_node("Level").add_child(bullet)
 	bullet.global_position = self.global_position
+
+func shoot_v_at_player(num_shots : int):
+	var player = get_tree().root.get_node("Level/Player")
+	for shotNum in range(num_shots):
+		var bullet = preload("res://Bullets/Flechette.tscn").instance()
+		var direction : Vector2 = (player.global_position - global_position).normalized()
+		if shotNum % 2 == 0:
+			var angle = direction.angle() + deg2rad(v_angle_step*shotNum/2)
+			bullet.direction = Vector2(cos(angle), sin(angle))
+		else:
+			var angle = direction.angle() - deg2rad(v_angle_step*(shotNum+1)/2)
+			bullet.direction = Vector2(cos(angle), sin(angle))
+		get_tree().root.get_node("Level").add_child(bullet)
+		bullet.global_position = self.global_position
--- Bullets/BulletStorm.gd
+++ Bullets/BulletStorm.gd
@@ -4,9 +4,15 @@ const bullet_scene = preload("res://Bullets/Bullet.tscn")
 onready var shoot_timer = $ShootTimer
 onready var rotater = $Rotater
 
+## Speed of bullet spawners spinning. Affects slope of spiral
 export(int) var rotate_speed = 100
+## Time between shots fired. Make this smaller to close
+##the distance between bullets
 export(float) var shoot_timer_wait_time = 0.1
+## Number of spawners that will be spinning around the enemy.
+## Increase this to have the spirals come closer together
 export(int) var spawn_point_count = 4
+## Distance from this node to where the bullets will be spawned.
 export(int) var radius = 10
 
 func start():
