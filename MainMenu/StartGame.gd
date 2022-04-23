6b3c76a8ea7fc67ea360d75ec2a539ce958f6b25
kingoftheconnors <Connormcp777@gmail.com>
2022-04-23 14:04:28 -0400

    Fix Main Menu to use Terra's scenes' new name

--- MainMenu/StartGame.gd
+++ MainMenu/StartGame.gd
@@ -4,4 +4,4 @@ func _on_Button_pressed():
 	print("Starting")
 	$AnimationPlayer.play("FadeToBlack")
 	yield(get_tree().create_timer(1), "timeout")
-	get_tree().change_scene("res://test.tscn")
+	get_tree().change_scene("res://Terra.tscn")
