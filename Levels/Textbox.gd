b9ef044a443775b5dd8b179e33653feced710eb8
kingoftheconnors <Connormcp777@gmail.com>
2022-04-23 21:44:50 -0400

    Add Base Pluto scene

--- Levels/Textbox.gd
+++ Levels/Textbox.gd
@@ -4,6 +4,7 @@ export(bool) var first_box = false
 export(String) var text = "Hello there"
 export(String) var color = "red"
 export(Array, NodePath) var nextBox
+export(float) var auto_advance_time = 0
 var active = false
 
 onready var animator = $VBoxContainer/AnimationPlayer
@@ -24,15 +25,21 @@ func start(skip = false):
 		return
 	$VBoxContainer/RichTextLabel.append_bbcode(
 		"[center][color=" + color + "]" + text + "[/color]")
-	animator.playback_speed = 20.0/text.length()
+	if text.length() > 0:
+		animator.playback_speed = 20.0/text.length()
 	animator.play("showBox")
 	active = true
+	if auto_advance_time > 0:
+		$Timer.start(auto_advance_time)
 
 func _input(event):
 	if active:
 		if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 			if animator.current_animation_position < animator.current_animation_length:
-				animator.advance((animator.current_animation_length - animator.current_animation_position)*text.length())
+				if text.length() > 0:
+					animator.advance((animator.current_animation_length - animator.current_animation_position)*text.length())
+				else:
+					animator.advance((animator.current_animation_length - animator.current_animation_position))
 			else:
 				end()
 
